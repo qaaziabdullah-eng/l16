ece2aab93ee3c7a9fb2572f92c0a21857369c7f3
qaaziabdullah-eng <qaaziabdullah@gmail.com>
2026-01-13 18:00:13 +0500

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,6 @@
+#break statement
+word=input("enter a word: ")
+for letter in word:
+    if letter=='a':
+        break 
+    print(letter)
---
+++ a3.py
@@ -0,0 +1,7 @@
+#continue
+var = 8
+while var > 0:
+    var = var - 1
+    if var ==4:
+        continue
+    print ('value:',var)
